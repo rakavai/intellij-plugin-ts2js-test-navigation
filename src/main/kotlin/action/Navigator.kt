685a9f8effbb358bd0e0da9f31c8f14480929f8c
Rakibul Hasan <rakibulh9@gmail.com>
2020-09-06 04:28:24 -0400

    Implement navigation

--- src/main/kotlin/action/Navigator.kt
+++ src/main/kotlin/action/Navigator.kt
@@ -1,9 +1,15 @@
 package action
 
+import com.intellij.openapi.fileEditor.OpenFileDescriptor
 import com.intellij.openapi.project.Project
+import com.intellij.psi.search.FilenameIndex.getVirtualFilesByName
+import com.intellij.psi.search.GlobalSearchScope.projectScope
 
 class Navigator {
-    fun navigateTo(project: Project, s: String) {
-
+    fun navigateTo(project: Project, fileName: String) {
+        val virtualFilesByName = getVirtualFilesByName(project, fileName, projectScope(project))
+        val firstVirtualFile = virtualFilesByName.iterator().next()
+        val descriptor = OpenFileDescriptor(project, firstVirtualFile)
+        descriptor.navigate(true)
     }
 }
